9842e07fe6b7263463d1828ada5caa12b26ffb7e
EC2 Default User <ec2-user@ip-172-31-35-207.us-east-2.compute.internal>
2020-03-16 14:53:39 +0000

    課題14提出

---
+++ app/Profiles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profiles extends Model
+{
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
